8278b98502fc4d893058219f14820e30753deb3e
Arpit Kumar Singh <singharpit302@gmail.com>
2023-07-18 00:28:41 +0530

    Tests created

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("./tasks/block-number");
+require("hardhat-gas-reporter");
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
@@ -16,7 +18,7 @@ module.exports = {
       chainId: 11155111,
     },
     localhost: {
-      url: "http://localhost:8545",
+      url: "http://127.0.0.1:8545/",
       chainId: 31337,
     },
   },
@@ -24,4 +26,11 @@ module.exports = {
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP,
+  }
 };
