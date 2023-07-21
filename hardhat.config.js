91eab8f79345eb294ab102c6ed8e90b0d48de2c0
Arpit Kumar Singh <singharpit302@gmail.com>
2023-07-21 20:20:26 +0530

    Completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,10 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("./tasks/block-number");
 require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -31,6 +33,6 @@ module.exports = {
     outputFile: "gas-report.txt",
     noColors: true,
     currency: "USD",
-    coinmarketcap: COINMARKETCAP,
+    coinmarketcap: COINMARKETCAP_API_KEY,
   }
 };
